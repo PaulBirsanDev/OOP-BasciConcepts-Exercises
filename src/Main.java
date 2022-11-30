cdb8c5d3e2af506c279066e1714ebb1270b2b960
Paul-Cristian Bîrsan <paulbirsan15@gmail.com>
2022-11-30 16:39:30 +0200

    Made some changes to the entire project, easier.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Prices prices = new Prices();
-        System.out.println(prices.getBaseBurgerPrice());
-        System.out.println(prices.getDeluxeBurgerPrice());
-        System.out.println(prices.getHealthyBurgerPrice());
+        Burger burger = new Burger("Basic", "Sausage", 3.56, "White");
+        double price = burger.itemizeBurger();
+        burger.addBurgerAddition1("Tomato",0.75);
+        burger.addBurgerAddition2("Lettuce",0.30);
+        burger.addBurgerAddition3("Cheese",1.12);
+        System.out.println("Total Burger price is " + burger.itemizeBurger());
     }
 }
